ef800f23f8dbc0e882625308c257753e5ec86679
sebgrgs <sebastien.georges11@gmail.com>
2025-03-25 14:24:05 +0100

    feat: add HolbertonCourse class with validation for properties and main file for testing

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(value) {
+    if (Array.isArray(value)) {
+      for (const i in value) {
+        if (typeof value[i] === 'string') {
+          this._students = value;
+        } else {
+          throw new TypeError('Students must be a string');
+        }
+      }
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+}
