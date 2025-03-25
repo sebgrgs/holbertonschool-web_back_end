d7abf6da1c397a1a767b21c50451e2dfee529e84
sebgrgs <sebastien.georges11@gmail.com>
2025-03-25 11:08:50 +0100

    fix: format constructor method in ClassRoom class for consistency

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-	constructor(maxStudents){
-		this._maxStudentsSize = maxStudents;
-	}
+  constructor(maxStudents) {
+    this._maxStudentsSize = maxStudents;
+  }
 }
