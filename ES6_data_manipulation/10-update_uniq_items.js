5e1681e927243fff3b0894978d3dc62088f5e059
sebgrgs <sebastien.georges11@gmail.com>
2025-04-01 15:20:45 +0200

    fix: correct indentation in updateUniqueItems and hasValuesFromArray functions

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(map) {
-	if (!(map instanceof Map)) {
-		throw new Error('Cannot process');
-	}
-	map.forEach((value, key) => {
-		if (value === 1) {
-			map.set(key, 100);
-		}
-	});
-	return map;
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+  return map;
 }
