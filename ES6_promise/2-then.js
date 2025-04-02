0da5909b6d4608209c7692174d843c7de2c5d551
sebgrgs <sebastien.georges11@gmail.com>
2025-04-02 18:20:50 +0200

    fix: correct indentation in handleResponseFromAPI function

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-	return new Promise((resolve, reject) => {
-		if (promise) {
-			resolve({
-				status: 200,
-				body: 'Success',
-			});
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+  });
 }
