2702f7e20db130ece69b690a960a1e6a1587c37e
sebgrgs <sebastien.georges11@gmail.com>
2025-04-03 12:59:18 +0200

    feat: implement various ES6 promise functions for user signup and API handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    }
-  });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
