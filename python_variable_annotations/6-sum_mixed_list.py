867bf76cab49735d42ae376336e2d000f58ba773
sebgrgs <sebastien.georges11@gmail.com>
2025-04-22 15:55:43 +0200

    fix: reorder type annotations in sum_mixed_list function for consistency

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,6 @@ and returns the sum of the list as a float.
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of the list as a float."""
     return sum(mxd_lst)
