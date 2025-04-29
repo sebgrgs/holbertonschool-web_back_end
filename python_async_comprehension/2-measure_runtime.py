7cb002bb03880695385184bffba4abd5be579a1b
sebgrgs <sebastien.georges11@gmail.com>
2025-04-29 09:37:59 +0200

    refactor: optimize measure_runtime function by using a loop to create tasks

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,12 +7,12 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Measure the runtime of async_comprehension."""
+    task = []
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
+    for _ in range(4):
+        task.append(asyncio.create_task(async_comprehension()))
+
+    await asyncio.gather(*task
     )
     end_time = time.time()
     return end_time - start_time
