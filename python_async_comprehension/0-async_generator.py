906857d925089f100460793a2124c0c313982cb8
sebgrgs <sebastien.georges11@gmail.com>
2025-04-29 09:33:34 +0200

    fix: update async_generator return type to use typing.Generator for consistency

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,9 @@
 """Async generator that yields a random number every second."""
 import random
 import asyncio
-from typing import AsyncGenerator
+import typing
 
-
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> typing.Generator[float, None, None]:
     """Yields a random number between 0 and 10 every second."""
     for _ in range(10):
         await asyncio.sleep(1)
