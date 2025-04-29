51c83c04e8fab86eb121cd8ecefb00421d821513
sebgrgs <sebastien.georges11@gmail.com>
2025-04-29 09:39:57 +0200

    fix: add missing newline for improved code readability

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ import random
 import asyncio
 import typing
 
+
 async def async_generator() -> typing.Generator[float, None, None]:
     """Yields a random number between 0 and 10 every second."""
     for _ in range(10):
