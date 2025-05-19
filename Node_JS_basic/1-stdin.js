7b69c58692d4fa776726a25d1dcbac6dac123c02
sebgrgs <sebastien.georges11@gmail.com>
2025-05-19 11:44:10 +0200

    Add package.json for Node.js project with dependencies and scripts

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+  });
+  
+  readline.question('Welcome to Holberton School, what is your name?', name => {
+	console.log(`Your name is: ${name}!`);
+	readline.close();
+  });
