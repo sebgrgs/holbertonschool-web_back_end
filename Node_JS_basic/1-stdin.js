aa91d91402a0d52196e3672cf5ef196ae6de5d82
sebgrgs <sebastien.georges11@gmail.com>
2025-05-19 23:05:34 +0200

    fix: correct formatting of console output in stdin script

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
 const readline = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout
-  });
-  
-  readline.question('Welcome to Holberton School, what is your name?', name => {
-	console.log(`Your name is: ${name}!`);
-	readline.close();
-  });
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  readline.close();
+});
+
+readline.on('close', () => {
+  console.log('This important software is now closing');
+});
