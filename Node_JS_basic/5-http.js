c9cce0f18f2d6237a9d887a7f4d62d5d9dd862c1
sebgrgs <sebastien.georges11@gmail.com>
2025-05-20 16:12:24 +0200

    fix: streamline response handling in 5-http.js for improved clarity and consistency

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,25 +4,24 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.set('Content-Type', 'text/plain');
-    res.write('Hello Holberton School!');
-    res.end();
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.set('Content-Type', 'text/plain');
-    res.write('This is the list of our students');
     countStudents(database)
       .then((output) => {
-        res.write(output);
-        res.end();
+        res.end(`This is the list of our students\n${output}`);
       })
       .catch((err) => {
-        res.write(err.message);
-        res.end();
+        res.end(`This is the list of our students\n${err.message}`);
       });
   } else {
     res.statusCode = 404;
     res.end('Not found');
   }
-}).listen(1245);
+});
+
+app.listen(1245);
 module.exports = app;
